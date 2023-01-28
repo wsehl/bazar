976d11b736b21da08b75367db213ea8ca4dd0700
prenl <elnur.kazakhstan@gmail.com>
2023-01-29 00:07:00 +0600

    change hello world

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! from Yelnur #2");
+        System.out.println("Hello, World! from Yelnur #3 windows");
     }
 }
