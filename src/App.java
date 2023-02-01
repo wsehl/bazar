ae41d3753ac11dacfa62112607617cdffd4c091d
Alex Sehl <widesehl@gmail.com>
2023-02-02 00:25:41 +0600

    mvp: db connection

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.sql.*;
+
 public class App {
+    static final String DB_URL = "jdbc:postgresql://195.49.215.164/marketplace";
+    static final String USER = "marketplace";
+    static final String PASSWORD = "marketplace";
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! from Yelnur #4 windows");
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                Statement stmt = conn.createStatement();) {
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
