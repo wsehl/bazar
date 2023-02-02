18700df89972e9793aa0dc22dfbe0f79fd4d2879
Yelnur <elnur.kazakhstan@gmail.com>
2023-02-02 20:40:04 +0600

    feat: clear console

--- src/App.java
+++ src/App.java
@@ -7,10 +7,11 @@ public class App {
         ProductController productController = new ProductController();
         Scanner scanner = new Scanner(System.in);
         boolean isActive = true;
+        clearConsole();
 
         while (isActive) {
             int input = 0;
-            System.out.println("\nChoose option:\n" +
+            System.out.println("Choose option:\n" +
                                 "1 - Output product\n" +
                                 "2 - Update product\n" +
                                 "3 - Add product\n" +
@@ -19,39 +20,55 @@ public class App {
             input = scanner.nextInt();
             
             if (input == 1) { // output product
+                clearConsole();
                 System.out.println("Choose option:\n" + 
                                     "1 - Output all products\n" +
-                                    "2 - Output product by ID");
+                                    "2 - Output product by ID\n" +
+                                    "3 - Back");
                 switch (scanner.nextInt()) {
-                    default: System.out.println("Wront input!"); break;
+                    default: System.out.println("Wront input!\n"); break;
                     case 1: { // output all products
+                        clearConsole();
                         productController.getProducts().forEach(System.out::println); 
                         break;
                     } 
                     case 2: { // output product by id
+                        clearConsole();
                         System.out.println("Enter product ID: ");
-                        System.out.println(productController.getProductById(scanner.nextInt())); break;
+                        int productId = -1;
+                        while (productId == -1) {
+                            productId = scanner.nextInt();
+                        }
+                        clearConsole();
+                        System.out.println(productController.getProductById(productId) + "\n"); break;
+                    }
+                    case 3: {
+                        clearConsole();
                     }
                 }
             } 
             else if (input == 2) { // update product
+                clearConsole();
                 Product updatedProduct;
                 String name;
                 double price;
                 String description;
                 int productId;
 
-                System.out.println("Enter product ID: ");
+                System.out.print("Enter product ID: ");
                 productId = scanner.nextInt();
-                System.out.println("Choose option:\n" +
+                clearConsole();
+                System.out.println("UPDATE PRODUCT: " + productId + "\nChoose option:\n" +
                                     "1 - Update name\n" +
                                     "2 - Update price\n" +
-                                    "3 - Update decription");
+                                    "3 - Update decription\n" +
+                                    "4 - Back");
 
                 switch (scanner.nextInt()) {
-                    default: System.out.println("Wrong input!"); break;
+                    default: System.out.println("Wrong input!\n"); break;
                     case 1: { // update name
-                        System.out.print("Enter new name: ");
+                        clearConsole();
+                        System.out.print("UPDATE PRODUCT: " + productId + "\nEnter new name: ");
                         name = "";
                         while (name == "") {
                             name = scanner.nextLine();
@@ -60,11 +77,12 @@ public class App {
                         updatedProduct = productController.getProductById(productId);
                         updatedProduct.setName(name);
                         productController.updateProduct(productId, updatedProduct);
-                        System.out.println("Product " + productId + " name changed to \"" + name + "\"");
+                        System.out.println("Product " + productId + " name changed to \"" + name + "\"\n");
                         break;
                     }
                     case 2: { // update price
-                        System.out.print("Enter new price: ");
+                        clearConsole();
+                        System.out.print("UPDATE PRODUCT: " + productId + "\nEnter new price: ");
                         price = -1;
                         while (price == -1) {
                             price = scanner.nextDouble();
@@ -73,11 +91,12 @@ public class App {
                         updatedProduct = productController.getProductById(productId);
                         updatedProduct.setPrice(price);
                         productController.updateProduct(productId, updatedProduct);
-                        System.out.println("Product " + productId + " price changed to \"" + price + "\"");
+                        System.out.println("Product " + productId + " price changed to \"" + price + "\"\n");
                         break;
                     }
                     case 3: { // update description
-                        System.out.print("Enter new description: ");
+                        clearConsole();
+                        System.out.print("UPDATE PRODUCT: " + productId + "\nEnter new description: ");
                         description = "";
                         while (description == "") {
                             description = scanner.nextLine();
@@ -86,12 +105,16 @@ public class App {
                         updatedProduct = productController.getProductById(productId);
                         updatedProduct.setDescription(description);
                         productController.updateProduct(productId, updatedProduct);
-                        System.out.println("Product " + productId + " description changed to \"" + description + "\"");
+                        System.out.println("Product " + productId + " description changed to \"" + description + "\"\n");
                         break;
+                    }
+                    case 4: {
+                        clearConsole();
                     }
                 }   
             }
             else if (input == 3) { // add product
+                clearConsole();
                 System.out.print("Enter new product's name: ");
                 String name = "";
                 while (name == "") {
@@ -112,41 +135,53 @@ public class App {
 
                 Product product = new Product(name, description, price);
                 productController.addProduct(product);
-                System.out.println("Product " + name + " has been added to database.");
+                clearConsole();
+
+                System.out.println("Product \"" + name + "\"  has been added to database.\n");
             }
             else if (input == 4) { // delete product
+                clearConsole();
                 System.out.print("Enter product id: ");
                 int productId = -1;
                 while (productId == -1) {
                     productId = scanner.nextInt();
                 }
 
-                System.out.print("Are you sure? (y/n): ");
+                System.out.print("DELETE PRODUCT: " + productId + "\nAre you sure? (y/n): ");
                 String confirmation = "";
                 while (confirmation == "") {
                     confirmation = scanner.nextLine();
                 }
 
                 switch (confirmation) {
-                    default: System.out.println("Wrong input!"); break;
+                    default: System.out.println("Wrong input!\n"); break;
                     case "y": {
                         productController.deleteProduct(productId);
-                        System.out.println("Product " + productId + " has been deleted successfully");
+                        clearConsole();
+                        System.out.println("Product " + productId + " has been deleted successfully\n");
                     }
                     case "n": {
+                        clearConsole();
                         break;
                     }
                 }
             }
             else if (input == 5) { // terminate loop
+                clearConsole();
                 isActive = false;
+                System.out.println("Marketplace CLI closed\n");
             }
             else {
-                System.out.println("Wrong input!");
+                clearConsole();
+                System.out.println("Wrong input!\n");
             }
         }
-
         scanner.close();
 
+    }
+
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
