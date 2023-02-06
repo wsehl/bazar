6a413d2d902a9c17070f0b44ebcb9e4cee32b6fa
prenl <elnur.kazakhstan@gmail.com>
2023-02-06 22:12:54 +0600

    feat: UserAlreadyExistsException handling

--- src/controllers/AuthController.java
+++ src/controllers/AuthController.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import exceptions.UserAlreadyExistsException;
 import models.User;
 import models.Controller;
 import utils.PBKDF2;
@@ -17,12 +18,23 @@ public class AuthController extends Controller {
 
     private final byte[] SALT = "salt".getBytes();
 
-    public User register(String firstName, String lastName, String email, String password, int roleId) {
+    public User register(String firstName, String lastName, String email, String password, int roleId) throws Exception {
         try {
+            String getUserSQL = "SELECT * FROM users WHERE email = ?";
+            PreparedStatement preparedStatement = getConnection().prepareStatement(getUserSQL);
+            preparedStatement.setString(1, email);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                if (resultSet.getString("email").equals(email)) {
+                    throw new UserAlreadyExistsException("User with this email already exists!");
+                }
+            }
+
             String hashedPassword = PBKDF2.bytesToHex(PBKDF2.getEncryptedPassword(password, SALT));
 
             String insertUserSQL = "INSERT INTO users (first_name, last_name, email, password, role_id) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = getConnection().prepareStatement(insertUserSQL,
+            preparedStatement = getConnection().prepareStatement(insertUserSQL,
                     PreparedStatement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1, firstName);
             preparedStatement.setString(2, lastName);
@@ -32,7 +44,7 @@ public class AuthController extends Controller {
             preparedStatement.executeUpdate();
 
             // Get the generated user ID
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 int userId = resultSet.getInt(1);
                 return new User(userId, firstName, lastName, email);
