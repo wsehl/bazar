8d326cf55a7e0a96e970c8abd354b49864a72589
Yelnur <elnur.kazakhstan@gmail.com>
2023-02-22 22:27:10 +0600

    feat: override tostring and new constructor

--- src/entities/Admin.java
+++ src/entities/Admin.java
@@ -1,7 +1,22 @@
 package entities;
 
 public class Admin extends User {
-    public Admin(String firstName, String lastName, String email, int roleId){
-        super(firstName, lastName, email, roleId);
+
+    public Admin(String firstName, String lastName, String email) {
+        super(firstName, lastName, email);
     }
+
+    public Admin(int id, String firstName, String lastName, String email) {
+        super(id, firstName, lastName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Admin {" +
+                "id = " + id +
+                ", first_name = '" + firstName + '\'' +
+                ", second_name = '" + lastName + '\'' +
+                ", email = '" + email + "'}";
+    }
+
 }
